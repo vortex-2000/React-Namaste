b259f4d7b32cb26b505664884ffdd1e44d990891
vortex-2000 <dasashutosh.2000@gmail.com>
2024-06-28 23:53:24 +0530

    EP1 Inception

---
+++ App.js
@@ -0,0 +1,17 @@
+
+
+    const parent = React.createElement
+    (
+        "div",
+        {id:"div"},
+        [
+            React.createElement("h1",{id:"h1kacolor1"},"H1 element of react"),
+            React.createElement("h1",{id:"h1kacolor2"},"H1 element of react sister")
+        ]
+    );
+
+    
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    
+    root.render(parent);
+
