d7d85143b05aa0759533d81e94d1f394f4ac8108
vortex-2000 <dasashutosh.2000@gmail.com>
2024-06-29 17:44:54 +0530

    test commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-
+import React from "react";
+import ReactDOM from "react-dom";
 
     const parent = React.createElement
     (
         "div",
         {id:"div"},
         [
-            React.createElement("h1",{id:"h1kacolor1"},"H1 element of react"),
+            React.createElement("h1",{id:"h1kacolor1"},"H1 elemen of react"),
             React.createElement("h1",{id:"h1kacolor2"},"H1 element of react sister")
         ]
     );
